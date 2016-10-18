dcd700dc75e1ad281d36fd3c0f7fe4721daff162
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-10-18 11:04:38 +0100

    Get req from out.global.events rather than out.stream .stream is undefined in newer versions of Marko.

--- taglib/i18n-var-tag.js
+++ taglib/i18n-var-tag.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = function (el, codegen) {
-  const builder = codegen.builder;
   const nameAttrValue = el.getAttributeValue('name');
   const name = nameAttrValue ? nameAttrValue.value : null;
-  return builder.var(name || 'i18n', 'out.stream.req.t');
+  return codegen.builder.var(name || 'i18n', 'out.global.events.req.t');
 };
